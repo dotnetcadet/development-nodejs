118e4e301ced5cf4ecd8d7ee32c8b3b3e04e0817
Chase Crawford <ccrawford@eastdilsecured.tech>
2022-09-24 17:22:39 -0400

    training apps

---
+++ vue/training-app-vite-tailwindcss/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">{{ environment }}</h1>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+  data() {
+    return {
+      environment: import.meta.env.VITE_APP_TITLE,
+    };
+  },
+});
+</script>
---
+++ vue/training-app-vite/src/App.vue
@@ -0,0 +1,36 @@
+  
+<template>
+  <div>
+    <h1>{{ environment }}</h1>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  setup() {
+
+  },
+  data() {
+    return {
+      environment: import.meta.env.VITE_APP_TITLE
+    }
+  }
+})
+</script>
+
+    
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
